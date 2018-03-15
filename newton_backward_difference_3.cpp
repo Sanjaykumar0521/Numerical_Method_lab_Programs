8ba16b5e97c2e1530b509684a269a13070c9e5eb
SKY <sonuyadav0521@gmail.com>
2018-03-15 15:57:42 +0530

    Backward_interpolation

---
+++ newton_backward_difference_3.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+float val_p(float p, int n)
+{
+    float temp = p;
+    for (int i = 1; i < n; i++)
+        temp = temp * (p + i);
+    return temp;
+}
+int fact(int n)
+{
+    int f = 1;
+    for (int i = 2; i <= n; i++)
+        f *= i;
+    return f;
+}
+
+
+int main()
+{
+    int i=0,n,j=0,k=0,m;
+    float p,y,value,f[10],t[10],a[10][10],x[10];
+    cout<<"enter total no. of values ";
+    cin>>n;
+    cout<<"enter values of x ";
+    for(i=0;i<n;i++)
+    {
+        cin>>t[i];
+    }
+    cout<<"enter the values of f(x) ";
+    for(i=0;i<n;i++)
+    {
+        cin>>f[i];
+    }
+    x[0]=f[n-1];k=k+1;
+    cout<<"enter the value of x at which the value is to find out ";
+    cin>>value;
+    for(m=0;m<n;m++)
+    {
+     for(i=0;i<n-m-1;i++)
+     {
+        f[i]=f[i+1]-f[i];
+        a[i][m]=f[i];
+        if(i==n-m-2)
+        {
+            x[k]=f[i];
+            k++;
+        }
+     }
+    }
+    cout<<"Table of values \n";
+  for(m=0;m<n;m++)
+    {
+    for(i=0;i<n-m-1;i++)
+        {
+         cout << setw(4) << a[m][i] << "\t";
+        }
+        cout << endl;
+    }
+  float sum = x[0];
+    float u = (value - t[n - 1]) / (t[1] - t[0]);
+    for (int i = 1; i < n; i++) {
+        sum = sum + (val_p(u, i) * x[i]) /fact(i);
+    }
+   cout << " the value at " << value << " is "
+         << sum << endl;
+
+    return 0;
+}
+
